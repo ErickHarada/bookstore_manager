1fc80bc74b65e45bafae6b66cd365324a41b50de
Erick <eefeaga2000@gmail.com>
2020-06-14 12:39:04 -0300

    First commit: project structure created

---
+++ src/test/java/com/erickharada/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickharada.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
